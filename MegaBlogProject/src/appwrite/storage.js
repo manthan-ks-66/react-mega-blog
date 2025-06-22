b62113a676f270cc35754ff9931753b749717b5e
Manthan Srivastava <manthanks.0606@gmail.com>
2025-06-22 22:52:20 +0530

    feat: enhance StorageService with CRUD operations and update config for articles collection

--- MegaBlogProject/src/appwrite/storage.js
+++ MegaBlogProject/src/appwrite/storage.js
@@ -1,5 +1,5 @@
 import config from "../config/config";
-import { ID, Databases, Storage, Client } from "appwrite";
+import { ID, Databases, Storage, Client, Query } from "appwrite";
 
 export class StorageService {
   client = new Client();
@@ -13,6 +13,80 @@ export class StorageService {
 
     this.storage = new Storage(this.client);
     this.databases = new Databases(this.client);
+  }
+
+  async createPost({ title, content, featuredImage, status, userId, slug }) {
+    try {
+      return await this.databases.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteArticlesCollectionId,
+        slug,
+        {
+          title,
+          content,
+          userId,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite error in creating post: ", error);
+    }
+  }
+
+  async updateDocument(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        config.appwriteDatabaseId,
+        config.appwriteArticlesCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite error in updating post: ", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        config.appwriteDatabaseId,
+        config.appwriteArticlesCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite error in deleting the document: ", error);
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteArticlesCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite error in getting the document: ", error);
+    }
+  }
+
+  async listAllPosts() {
+    try {
+      return await this.databases.listDocuments(
+        config.appwriteDatabaseId,
+        config.appwriteArticlesCollectionId,
+        [Query.equal("status", "active")]
+      );
+    } catch (error) {
+      console.log("Appwrite error in listing the post: ", error);
+      return false;
+    }
   }
 }
 
--- MegaBlogProject/src/config/config.js
+++ MegaBlogProject/src/config/config.js
@@ -2,7 +2,7 @@ const config = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
-  appwriteArticlesId: String(import.meta.env.VITE_ARTICLES_ID),
+  appwriteArticlesCollectionId: String(import.meta.env.VITE_ARTICLES_ID),
   appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
 };
 
